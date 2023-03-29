c8941f471a468df3bb89b97762dd57423a5576c0
Dante Sack <dante.sack@leniolabs.com>
2023-03-29 17:36:46 -0300

    setup base project with create-next-app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,6 @@
-import Script from "next/script";
-import { SessionProvider } from "next-auth/react";
-import { AppProps } from "next/app";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return (
-    <>
-      <SessionProvider session={pageProps.session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-      <Script
-        src="https://accounts.google.com/gsi/client"
-        strategy="afterInteractive"
-      />
-    </>
-  );
-};
-
-export default App;
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
